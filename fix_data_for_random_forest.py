7a87c2fc083f5b7be52ce3f73947590e383adb66
rabbitt <rabbitt1991x@gmail.com>
2023-01-19 23:06:52 +0330

    random forest class update

--- fix_data_for_random_forest.py
+++ fix_data_for_random_forest.py
@@ -9,108 +9,109 @@ import os ,datetime
 
 
 
-def fill_dic(input_list, out_dic, csv_file_name, row_name):
-    for index, i in enumerate(csv_file_name[row_name]):
-        dic_key = i
-        if type(dic_key) == float:
-            dic_key = f'None{index}'
+# def fill_dic(input_list, out_dic, csv_file_name, row_name):
+#     for index, i in enumerate(csv_file_name[row_name]):
+#         dic_key = i
+#         if type(dic_key) == float:
+#             dic_key = f'None{index}'
 
-        out_dic[dic_key] = input_list[index]
+#         out_dic[dic_key] = input_list[index]
 
-    return out_dic
-
-
-def save_to_csv(car_info, csv_file_name):
-        df = pd.DataFrame.from_dict(car_info, orient='index', )
-        df = df.transpose()
-        if os.path.isfile(f'{csv_file_name}.csv'):df.to_csv(f'{csv_file_name}.csv',mode='a',header=False, index=0)
-        else:df.to_csv(f'{csv_file_name}.csv',index=0)
+#     return out_dic
 
 
-car_brand_dic = {}
-car_model_dic = {}
-car_gear_dic = {}
-car_city_dic = {}
-car_address_dic = {}
-car_date_dic = {}
-
-with_price = pd.read_csv("CAR_INFO_DB_with_price.csv")
-without_price = pd.read_csv("CAR_INFO_DB_without_price.csv")
-
-le = preprocessing.LabelEncoder()
+# def save_to_csv(car_info, csv_file_name):
+#         df = pd.DataFrame.from_dict(car_info, orient='index', )
+#         df = df.transpose()
+#         if os.path.isfile(f'{csv_file_name}.csv'):df.to_csv(f'{csv_file_name}.csv',mode='a',header=False, index=0)
+#         else:df.to_csv(f'{csv_file_name}.csv',index=0)
 
 
-new_date = with_price['date']
-new_date_list = []
-for date_time in new_date :
+# car_brand_dic = {}
+# car_model_dic = {}
+# car_gear_dic = {}
+# car_city_dic = {}
+# car_address_dic = {}
+# car_date_dic = {}
 
-    if len(date_time.split('-')) < 3 :
-        new_date_time =  str(datetime.datetime.today()).split()[0]
-    else: new_date_time = date_time
-    new_date_time = new_date_time.split('-')
+# with_price = pd.read_csv("CAR_INFO_DB_with_price.csv")
+# without_price = pd.read_csv("CAR_INFO_DB_without_price.csv")
 
-    new_date_time = new_date_time[0] + new_date_time[1] + new_date_time[2]
-    print(new_date_time)
-    new_date_list.append(int(new_date_time))
-
-car_brand = with_price['car_brand']
-le.fit(car_brand)
-car_brand = le.transform(car_brand)
-car_brand_dic = fill_dic(car_brand, car_brand_dic, with_price, 'car_brand')
-car_model = with_price['car_model']
-le.fit(car_model)
-car_model = le.transform(car_model)
-car_model_dic = fill_dic(car_model, car_model_dic, with_price, 'car_model')
-year = with_price['year']
-km = with_price['km']
-gear = with_price['gear']
-le.fit(gear)
-gear = le.transform(gear)
-car_gear_dic = fill_dic(gear, car_gear_dic, with_price, 'gear')
-city = with_price['city']
-le.fit(city)
-city = le.transform(city)
-car_city_dic = fill_dic(city, car_city_dic, with_price, 'city')
-address = with_price['address']
-le.fit(address)
-address = le.transform(address)
-car_address_dic = fill_dic(address, car_address_dic, with_price, 'address')
-price = with_price['price']
-
-mydic = {}
-for index, data in enumerate(with_price['id']):
-    mydic[data] =[car_brand[index] ,car_model[index], year[index], km[index], gear[index], city[index],price[index],new_date_list[index]]
+# le = preprocessing.LabelEncoder()
 
 
+# new_date = with_price['date']
+# new_date_list = []
+# for date_time in new_date :
 
-    car_data = {
-        'id': data,
-        'date': new_date_list[index],
-        'car_brand': car_brand[index],
-        'car_model': car_model[index],
-        'year': year[index],
-        'km': km[index],
-        'gear': gear[index],
-        'city': city[index],
-        'price': price[index],
+#     if len(date_time.split('-')) < 3 :
+#         new_date_time =  str(datetime.datetime.today()).split()[0]
+#     else: new_date_time = date_time
+#     new_date_time = new_date_time.split('-')
 
-    }
-    save_to_csv(car_data, 'to_use_for_random_forest_without_price')
+#     new_date_time = new_date_time[0] + new_date_time[1] + new_date_time[2]
+#     print(new_date_time)
+#     new_date_list.append(int(new_date_time))
+
+# car_brand = with_price['car_brand']
+# le.fit(car_brand)
+# car_brand = le.transform(car_brand)
+# car_brand_dic = fill_dic(car_brand, car_brand_dic, with_price, 'car_brand')
+# car_model = with_price['car_model']
+# le.fit(car_model)
+# car_model = le.transform(car_model)
+# car_model_dic = fill_dic(car_model, car_model_dic, with_price, 'car_model')
+# year = with_price['year']
+# km = with_price['km']
+# gear = with_price['gear']
+# le.fit(gear)
+# gear = le.transform(gear)
+# car_gear_dic = fill_dic(gear, car_gear_dic, with_price, 'gear')
+# city = with_price['city']
+# le.fit(city)
+# city = le.transform(city)
+# car_city_dic = fill_dic(city, car_city_dic, with_price, 'city')
+# address = with_price['address']
+# le.fit(address)
+# address = le.transform(address)
+# car_address_dic = fill_dic(address, car_address_dic, with_price, 'address')
+# price = with_price['price']
+
+# mydic = {}
+# for index, data in enumerate(with_price['id']):
+#     mydic[data] =[car_brand[index] ,car_model[index], year[index], km[index], gear[index], city[index],price[index],new_date_list[index]]
+
+
+
+#     car_data = {
+#         'id': data,
+#         'date': new_date_list[index],
+#         'car_brand': car_brand[index],
+#         'car_model': car_model[index],
+#         'year': year[index],
+#         'km': km[index],
+#         'gear': gear[index],
+#         'city': city[index],
+#         'price': price[index],
+
+#     }
+#     save_to_csv(car_data, 'to_use_for_random_forest_without_price')
 
 
 
 class data_fixer:
     def __init__(self):
-        self.car_brand_dic = {}
-        self.car_model_dic = {}
-        self.car_gear_dic = {}
-        self.car_city_dic = {}
-        self.car_address_dic = {}
-        self.car_date_dic = {}
+        self.car_brand = []
+        self.car_model = []
+        self.car_gear = []
+        self.car_city = []
+        self.car_address = []
+        self.car_date = []
         self.new_date_list = []
         self.with_price = pd.read_csv("CAR_INFO_DB_with_price.csv")
         self.without_price = pd.read_csv("CAR_INFO_DB_without_price.csv")
         self.le = preprocessing.LabelEncoder()
+
 
     def fill_dic(self,csv_file,out_dic,input_list,row_name):
         for index, i in enumerate(csv_file[row_name]):
@@ -121,32 +122,60 @@ class data_fixer:
             out_dic[dic_key] = input_list[index]
         return out_dic
     
-    def lables_data(self,csv_title_string,):    
+    def save_to_csv(car_info, csv_file_name):
+        df = pd.DataFrame.from_dict(car_info, orient='index', )
+        df = df.transpose()
+        if os.path.isfile(f'{csv_file_name}.csv'):df.to_csv(f'{csv_file_name}.csv',mode='a',header=False, index=0)
+        else:df.to_csv(f'{csv_file_name}.csv',index=0)
+
+    def lables_data(self,csv_title_string,csv_file):    
         csv_title_string = 'date'
-
-        new_date = with_price['date']
-
-        for date_time in new_date :
-
-            if len(date_time.split('-')) < 3 :
-                new_date_time =  str(datetime.datetime.today()).split()[0]
-            else: new_date_time = date_time
-            new_date_time = new_date_time.split('-')
-
-            new_date_time = new_date_time[0] + new_date_time[1] + new_date_time[2]
-            print(new_date_time)
-            new_date_list.append(int(new_date_time))
-
-        car_brand = with_price['car_brand']
-        le.fit(car_brand)
-        car_brand = le.transform(car_brand)
-        car_brand_dic = fill_dic(car_brand, car_brand_dic, with_price, 'car_brand')
+        if csv_title_string == 'date':    
+            new_date = with_price['date']
+            for date_time in new_date :
+                new_date_time = new_date_time.split('-')
+                new_date_time = new_date_time[0] + new_date_time[1] + new_date_time[2]
+                self.new_date_list.append(int(new_date_time))
+            return self.new_date_list
+        elif csv_title_string == 'km':
+            column_data = with_price['km']
+            return column_data
+        elif csv_title_string =='year':    
+            column_data = with_price['year']
+            return column_data
+        else:
+            column_data = csv_file[csv_title_string]
+            self.le.fit(column_data)
+            column_data = self.le.transform(column_data)
+            output_dic = self.fill_dic(column_data, output_dic, csv_file, csv_title_string)
+            return [output_dic , column_data]
     
-    def save_to_csv(self):
-        pass
-    
-    def fix_data(self):
-        pass
+    def fix_data(self,car_brand,car_model,year,km,gear,city,price,new_date_list,csv_file,output_csv_file_name_string):
+        for index, data in enumerate(csv_file['id']):
+            car_data = {
+                'id': data,
+                'date': new_date_list[index],
+                'car_brand': car_brand[index],
+                'car_model': car_model[index],
+                'year': year[index],
+                'km': km[index],
+                'gear': gear[index],
+                'city': city[index],
+                'price': price[index],
+            }
+            self.save_to_csv(car_data, output_csv_file_name)
+
+    def main(self):
+        self.car_brand = self.lables_data('car_brand',self.with_price)
     
     def __del__(self):
-        pass
+        self.car_brand_dic = None
+        self.car_model_dic = None
+        self.car_gear_dic = None
+        self.car_city_dic = None
+        self.car_address_dic = None
+        self.car_date_dic = None
+        self.new_date_list = None
+        self.with_price = None
+        self.without_price = None
+        self.le = None
