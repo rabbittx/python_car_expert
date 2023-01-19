1767546629bc53a4c3c7f094cc33562968787017
rabbitt <rabbitt1991x@gmail.com>
2023-01-20 00:18:23 +0330

    random forest ready to test

--- random_forest.py
+++ random_forest.py
@@ -64,14 +64,55 @@ pickle.dump(RFmodel,open(file_name,'wb'))
 
 
 class random_forest():
-    def __init__(self):
-        pass
+    def __init__(self):       
+        self.with_price = pd.read_csv("to_use_for_random_forest_with_price.csv")
+        self.without_price = pd.read_csv("to_use_for_random_forest_without_price.csv")
+        self.cars_without_labels = None
+        self.price = None
+        self.data_train = None 
+        self.data_test = None
+        self.labels_train = None 
+        self.labels_test = None
+        self.RFmodel = None
+        self.rf_pred_label = None
+        random.seed(100)
 
-    def make_df(self):
-        pass
+    def fill_dic(self,csv_file,input_list,row_name):
+        out_dic = {}
+        for index, i in enumerate(csv_file[row_name]):
+            dic_key = i
+            if type(dic_key) == float:
+                dic_key = f'None{index}'
+
+            out_dic[dic_key] = input_list[index]
+        return out_dic
     
+    def save_to_csv(self,car_info, csv_file_name):
+        df = pd.DataFrame.from_dict(car_info, orient='index', )
+        df = df.transpose()
+        if os.path.isfile(f'{csv_file_name}.csv'):df.to_csv(f'{csv_file_name}.csv',mode='a',header=False, index=0)
+        else:df.to_csv(f'{csv_file_name}.csv',index=0)
+
+    def make_df(self): 
+        self.with_price = self.with_price.append(self.without_price)
+        self.with_price = self.with_price.drop(self.with_price.columns[0],axis=1)
+        self.with_price = self.with_price.sample(frac=1).reset_index(drop=True)
+        self.cars_without_labels = self.with_price.drop('price',axis=1)
+        self.price = with_price['price']
+        
+
+            
     def train_model(self):
-        pass
-    
+        self.data_train, self.data_test, self.labels_train, self.labels_test = train_test_split(cars_without_labels, price, test_size=0.20, random_state=100)
+        self.RFmodel = RandomForestClassifier()
+        self.RFmodel.fit(self.data_train, self.labels_train)
+        self.rf_pred_label = RFmodel.predict(self.data_test)
+
     def check_price(self):
-        pass
+        cm2 = confusion_matrix(self.labels_test,self.rf_pred_label)
+        print(cm2)
+        print(accuracy_score(self.labels_test,self.rf_pred_label))
+
+    def save_random_frost(self):
+        file_name = "RandomForestModel.sav"
+        pickle.dump(RFmodel,open(file_name,'wb'))
